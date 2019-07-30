204c28c6c32cc867cb709c929d588d9b3da1b8dc
DevMcGann <gab.mcganicus@gmail.com>
2019-07-29 22:41:24 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Suscriptores from './componentes/suscriptores/Suscriptores'
+import NuevoSuscriptor from './componentes/suscriptores/NuevoSuscriptor'
+import MostrarSuscriptor from './componentes/suscriptores/MostrarSuscriptor'
+import EditarSuscriptor from './componentes/suscriptores/EditarSuscriptor'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Switch>
+              <Route exact path="/suscriptores" component={Suscriptores}/>
+              <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor}/>
+              <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor}/>
+              <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor}/>
+          </Switch>
+      </Router>
   );
 }
 
---
+++ src/componentes/suscriptores/MostrarSuscriptor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MostrarSuscriptor = () => {
+    return (  
+
+        <h1>Mostrar suscriptor</h1>
+    );
+}
+ 
+export default MostrarSuscriptor;
