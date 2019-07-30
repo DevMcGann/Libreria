8231245f1e5627973089f2791d6da5344c1e4f7c
DevMcGann <dev.mcgann@gmail.com>
2019-07-30 18:52:26 -0300

    1852

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Suscriptores from './componentes/suscriptores/Suscriptores'
 import NuevoSuscriptor from './componentes/suscriptores/NuevoSuscriptor'
 import MostrarSuscriptor from './componentes/suscriptores/MostrarSuscriptor'
 import EditarSuscriptor from './componentes/suscriptores/EditarSuscriptor'
+import Navbar from './componentes/layout/Navbar'
 
 
 
 function App() {
-  return (
-      <Router>
-          <Switch>
-              <Route exact path="/suscriptores" component={Suscriptores}/>
-              <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor}/>
-              <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor}/>
-              <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor}/>
-          </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <div className="container">
+                <Switch>
+                    <Route exact path="/suscriptores" component={Suscriptores} />
+                    <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+                    <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+                    <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
